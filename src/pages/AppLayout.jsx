7fb53ab4b6e634df82f7f50f9f33eac2a3d5938f
Emmanuel Abia <emmanuel.abia32@gmail.com>
2025-06-12 10:58:02 -0400

    Pages built

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
-import AppNav from "../components/AppNav";
+import AppNav from "../components/AppNav"
+
 
 export default function AppLayout() {
   return (
     <div>
         <AppNav />
+        <p>App</p>
     </div>
   )
 }
